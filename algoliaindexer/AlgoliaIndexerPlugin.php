039f644888c0e7c96a2f0a7c08eb7489f7a0471b
davidpanaho <davidpanaho@gmail.com>
2018-02-06 08:44:12 +0000

    Setup base plugin class

---
+++ algoliaindexer/AlgoliaIndexerPlugin.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Craft;
+
+class AlgoliaIndexerPlugin extends BasePlugin
+{
+    function getName()
+    {
+         return 'Algolia Indexer';
+    }
+
+    function getVersion()
+    {
+        return '1.0.0';
+    }
+
+    function getDeveloper()
+    {
+        return 'David Panaho';
+    }
+
+    function getDeveloperUrl()
+    {
+        return 'https://www.davidpanaho.com';
+    }
+}
