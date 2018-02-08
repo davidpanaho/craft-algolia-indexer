ccf0608d61ec7f39896225c4c5fad6ccaf5b789b
davidpanaho <davidpanaho@gmail.com>
2018-02-08 15:55:51 +0000

    Add single user indexing and refactor user transformation

--- algoliaindexer/services/AlgoliaIndexerService.php
+++ algoliaindexer/services/AlgoliaIndexerService.php
@@ -18,6 +18,19 @@ class AlgoliaIndexerService extends BaseApplicationComponent
         return $this->algoliaClient;
     }
 
+    public function indexUser($id)
+    {
+        $user = craft()->users->getUserById($id);
+
+        if ($user) {
+            $record = $this->transformUser($user);
+
+            $indexName = craft()->config->get('userIndex', 'algoliaindexer');
+            $index = $this->getAlgoliaClient()->initIndex($indexName);
+            $res = $index->addObject($record);
+        }
+    }
+
     public function reIndexUsers()
     {
         $indexName = craft()->config->get('userIndex', 'algoliaindexer');
@@ -37,40 +50,7 @@ class AlgoliaIndexerService extends BaseApplicationComponent
         $records = [];
 
         foreach ($users as $key => $user) {
-            $tags = [];
-
-            foreach ($user->userSpecialities as $key => $value) {
-                $tags[] = $value->title;
-            }
-
-            $images = [];
-            $profileImage = $user->userProfileImage;
-            foreach ($profileImage as $image) {
-                $images['small'] = $image->getUrl('small');
-                $images['medium'] = $image->getUrl('medium');
-                $images['large'] = $image->getUrl('large');
-                $images['original'] = $image->url;
-            }
-
-            $record = [
-                'objectID' => $user->id,
-                'id' => $user->id,
-                'firstName' => $user->firstName,
-                'lastName' => $user->lastName,
-                'jobTitle' => $user->userJobTitle,
-                'phone' => $user->userPhone,
-                'email' => $user->email,
-                'profileImages' => $images,
-            ];
-
-            // If the user doesn't have a bio, give them an empty one
-            if ($user->userBio) {
-                $bio = $user->userBio->getRawContent();
-            } else {
-                $bio = '';
-            }
-            $record['bio'] = $bio;
-
+            $record = $this->transformUser($user);
             $records[] = $record;
         }
 
@@ -81,11 +61,79 @@ class AlgoliaIndexerService extends BaseApplicationComponent
     {
         $index = $this->getAlgoliaClient()->initIndex($indexName);
         $res = $index->addObjects($records);
+
+        $this->addReplicaIndices($index, $indexName);
     }
 
     public function clearIndex($indexName)
     {
         $index = $this->getAlgoliaClient()->initIndex($indexName);
         $index->clearIndex();
+    }
+
+    public function addReplicaIndices($index, $indexName)
+    {
+        // TODO: the settings should be put somewhere else. Maybe in config file?
+        $replicas = [$indexName . '_firstName_asc'];
+        $index->setSettings([
+            'replicas' => $replicas,
+        ]);
+
+        $client = $this->getAlgoliaClient();
+
+        $indexFirstNameAsc = $client->initIndex($replicas[0]);
+        $indexFirstNameAsc->setSettings([
+            'ranking' => [
+                'asc(firstName)',
+                'typo',
+                'geo',
+                'words',
+                'filters',
+                'proximity',
+                'attribute',
+                'exact',
+                'custom',
+            ],
+        ]);
+    }
+
+    public function transformUser($user)
+    {
+        $tags = [];
+
+        foreach ($user->userSpecialities as $key => $value) {
+            $tags[] = $value->title;
+        }
+
+        $images = [];
+        $profileImage = $user->userProfileImage;
+        foreach ($profileImage as $image) {
+            $images['small'] = $image->getUrl('small');
+            $images['medium'] = $image->getUrl('medium');
+            $images['large'] = $image->getUrl('large');
+            $images['original'] = $image->url;
+        }
+
+        $record = [
+            'objectID' => $user->id,
+            'id' => $user->id,
+            'firstName' => $user->firstName,
+            'lastName' => $user->lastName,
+            'jobTitle' => $user->userJobTitle,
+            'phone' => $user->userPhone,
+            'email' => $user->email,
+            'profileImages' => $images,
+            'specialities' => $tags,
+        ];
+
+        // If the user doesn't have a bio, give them an empty one
+        if ($user->userBio) {
+            $bio = $user->userBio->getRawContent();
+        } else {
+            $bio = '';
+        }
+        $record['bio'] = $bio;
+
+        return $record;
     }
 }
